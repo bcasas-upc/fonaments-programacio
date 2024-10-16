f812c643f3a07f03e7523660bd3c52f5d7eb331f
bcasas-upc <64041242+bcasas-upc@users.noreply.github.com>
2024-10-16 15:59:41 +0200

    Update activitat1-amstrong.cc

--- activitats/activitat1-amstrong.cc
+++ activitats/activitat1-amstrong.cc
@@ -7,66 +7,53 @@ using namespace std;
 // Pre: n és un nombre natural
 // Post: retorna la longitud de n
 int longitud(int n) {
-	int l = 0;
-	bool continua = true;
-
-	while(continua) {
-		// Inv: l conté la longitud del nombre n
-		n = n / 10;
-		l++;
-		if (n == 0) {
-			continua = false;
-		}
+	int l = 1;
+	while(n > 9) {
+		// Inv: "l" conté la quantitat de xifres de “n" que s’han processat fins al moment;
+		//    “n" conté l’entrada original sense les “l” xifres menys significatives
+		n /= 10;
+		++l;
 	}
 	return l;
 }
 
-// Calcula la suma d'amstrong d'un nombre n. La suma d'amstrong és la suma 
+// Calcula la suma d'armstrong d'un nombre n. La suma d'armstrong és la suma 
 // de les potències n-èsimes dels seus dígits on n és la longitud del nombre.
 // Pre: n és un nombre natural
-// Post: retorna la suma d'amnstrong de n
-int suma_amstrong(int n) {
-	int d;
+// Post: retorna la suma d'armstrong de n
+int suma_armstrong(int n) {
 	int suma = 0;
-	bool continua = true;
 	int l = longitud(n);
 
-	while(continua) {
-		// Inv: suma conté la suma d'amnstrong del nombre n
-		d = n % 10;
-		suma = suma + pow(d, l);
+	while(n != 0) {
+		// Inv: suma conté la suma d'armstrong del nombre n
+		int d = n % 10;
+		suma += pow(d, l);
 		n = n / 10;
-		if(n == 0) {
-			continua = false;
-		}
 	}
 
 	return suma;
 }
 
 int main() {
-	int n, s;
+	int n;
 	bool continua_llegint = true;
 	bool algun_amstrong = false;
-
-	while(continua_llegint) {
+	cin >> n;
+	while (n != 0) {
 		// Inv: algun_amstrong indica si s'ha trobat un nombre amstrong o no
-		cin >> n;
-		if (n > 0) {
-			s = suma_amstrong(n);
-			if ( n == s) {
-				if(algun_amstrong) {
-					//vol dir que no és el primer, per tant, 
-					//poso un espai en blanc de separador
-					cout << " ";
-				} else {
-					algun_amstrong = true;
-				}
-				cout << n;
+		int s = suma_armstrong(n);
+		if (n == s) {
+			if(algun_amstrong) {
+				//vol dir que no és el primer, per tant, 
+				//poso un espai en blanc de separador
+				cout << " ";
+			} else {
+				algun_amstrong = true;
 			}
-		} else {
-			continua_llegint = false;
+			cout << n;
 		}
+		cin >> n;
 	}
 
 	if(not algun_amstrong) {
